1e7ccf10c154defe2eaaad8dea96b8a08c15b5c3
Muhd Danish <mdanish1908@gmail.com>
2019-08-20 00:07:31 +0800

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -2,16 +2,27 @@ def sort(images):
     result = []
     prevItem = None
 
+    i = 0
+    total_x = 0
+    total_y = 0
+
     for img in images:
         size = (img.width(), img.height())
         item = SortItem(img, size)
+        
         if prevItem is not None:
-            item.pos = (item.pos[0], prevItem.size[1] + prevItem.pos[1])
+            _item = prevItem
+            item.pos = (_item.size[0] + _item.pos[0], item.pos[1])
 
         result.append(item)
         prevItem = item
+        
+        i += 1
 
-    return tuple(result)
+    return [
+        tuple(result),
+        (total_x, total_y)
+    ]
 
 class SortItem:
     def __init__(self, img, size=(0, 0), pos=(0, 0)):
