fc65f98780929821f3ff536eca878733a277bd8e
chantw <34728894+chantw@users.noreply.github.com>
2019-08-19 09:28:03 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py-snap-collage',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='', install_requires=['mss', 'Pillow', 'PyQt5>=5.13.0', 'pyscreenshot']
+)
